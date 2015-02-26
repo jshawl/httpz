a20f4f03fba4c693305f4d09a733ed8fae5d669d
Jesse Shawl <jesse@jshawl.com>
2015-02-25 19:40:40 -0500

    add jsonview

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ var socket = io('http://localhost:3030');
 socket.on('request', function (data) {
   $('.requests').prepend( data );
   $('.js-timeago').timeago();
+  $(".js-jsonview").JSONView( JSON.parse($('.js-jsonview').html()) );
 });
 
 $('.js-timeago').timeago();
+$(".js-jsonview").JSONView( JSON.parse($('.js-jsonview').html()) );
