f98ced0f955a66e49840d8158d1826dd42cdd9cf
Jesse Shawl <jesse@jshawl.com>
2015-02-27 09:07:50 -0500

    refactor

--- config/routes.js
+++ config/routes.js
@@ -3,28 +3,55 @@ var fs = require('fs');
 var Handlebars = require('handlebars');
 
 module.exports = function( io ){
+  var handler = function handler( req, res ){
+    console.log( req.method );
+    Appointment.findOne({_id: req.params.id}, function( err, apt ){
+      if( err ) {
+	res( err );
+	return;
+      }
+      var request = {
+	headers: req.headers, 
+	method: req.method,
+	payload: req.payload,
+	createdAt: new Date().toISOString(),
+	id: apt._id,
+      }
+      apt.requests.push( request );
+      apt.save();
+      fs.readFile('views/request.html', 'utf8', function (err,data) {
+	if (err) {
+	  return console.log(err);
+	}
+	var template = Handlebars.compile(data);
+	var r = template( request );
+	io.to( req.params.id ).emit('request', r );
+      });
+      res( request )
+    })
+  }
    return [
 	{
-	    method: 'GET',
-	    path:'/', 
-	    handler: function (request, reply) {
-	       reply.view('index');
-	    }
+	  method: 'GET',
+	  path:'/', 
+	  handler: function (request, reply) {
+	     reply.view('index');
+	  }
 	},
 	{
-	    method: 'GET',
-	    path:'/{id}', 
-	    handler: function (request, reply) {
-	      Appointment.findOne({ _id: request.params.id }, function(err, apt) {
-		if (err) {
-		  reply(err);
-		  return;
-		}
-		apt.requests = apt.requests.reverse()
-		apt.host = request.info.host;
-		reply.view('show', {data: apt});
-	      });
-	    }
+	  method: 'GET',
+	  path:'/{id}', 
+	  handler: function (request, reply) {
+	    Appointment.findOne({ _id: request.params.id }, function(err, apt) {
+	      if (err) {
+		reply(err);
+		return;
+	      }
+	      apt.requests = apt.requests.reverse()
+	      apt.host = request.info.host;
+	      reply.view('show', {data: apt});
+	    });
+	  }
 	},
 	{
 	  method: 'GET',
@@ -38,141 +65,40 @@ module.exports = function( io ){
 	  }
 	},
 	{
-	    method: 'GET',
-	    path:'/appointments/create', 
-	    handler: function (req, res) {
-	      var apt = new Appointment({
-	        createdAt: new Date()
-	      })
-	      apt.save(function( err, apt ){
-		if( err ) {
-		  res( err );
-		  return;
-		}
-		res.redirect('/'+ apt._id );
-	      })
-	    }
+	  method: 'GET',
+	  path:'/appointments/create', 
+	  handler: function (req, res) {
+	    var apt = new Appointment({
+	      createdAt: new Date()
+	    })
+	    apt.save(function( err, apt ){
+	      if( err ) {
+		res( err );
+		return;
+	      }
+	      res.redirect('/'+ apt._id );
+	    })
+	  }
 	},
 	{
 	  method: 'POST',
 	  path: '/{id}',
-	  handler: function( req, res ){
-	    Appointment.findOne({_id: req.params.id}, function( err, apt ){
-	      if( err ) {
-		res( err );
-	        return;
-	      }
-	      var request = {
-			headers: req.headers, 
-	                method: "POST",
-			payload: req.payload,
-			createdAt: new Date().toISOString(),
-	        id: apt._id,
-	      }
-	      apt.requests.push( request );
-	      apt.save();
-	      fs.readFile('views/request.html', 'utf8', function (err,data) {
-		if (err) {
-		  return console.log(err);
-		}
-		var template = Handlebars.compile(data);
-		var r = template( request );
-		io.to( req.params.id ).emit('request', r );
-	      });
-	      res( request )
-	    })
-	  }
+	  handler: handler
 	},
-
 	{
 	  method: 'PUT',
 	  path: '/{id}',
-	  handler: function( req, res ){
-	    Appointment.findOne({_id: req.params.id}, function( err, apt ){
-	      if( err ) {
-		res( err );
-	        return;
-	      }
-	      var request = {
-			headers: req.headers, 
-	                method: "PUT",
-			payload: req.payload,
-			createdAt: new Date().toISOString(),
-	        id: apt._id,
-	      }
-	      apt.requests.push( request );
-	      apt.save();
-	      fs.readFile('views/request.html', 'utf8', function (err,data) {
-		if (err) {
-		  return console.log(err);
-		}
-		var template = Handlebars.compile(data);
-		var r = template( request );
-		io.to( req.params.id ).emit('request', r );
-	      });
-	      res( request )
-	    })
-	  }
+	  handler: handler
 	},
 	{
 	  method: 'PATCH',
 	  path: '/{id}',
-	  handler: function( req, res ){
-	    Appointment.findOne({_id: req.params.id}, function( err, apt ){
-	      if( err ) {
-		res( err );
-	        return;
-	      }
-	      var request = {
-			headers: req.headers, 
-	                method: "PATCH",
-			payload: req.payload,
-			createdAt: new Date().toISOString(),
-	        id: apt._id,
-	      }
-	      apt.requests.push( request );
-	      apt.save();
-	      fs.readFile('views/request.html', 'utf8', function (err,data) {
-		if (err) {
-		  return console.log(err);
-		}
-		var template = Handlebars.compile(data);
-		var r = template( request );
-		io.to( req.params.id ).emit('request', r );
-	      });
-	      res( request )
-	    })
-	  }
+	  handler: handler
 	},
 	{
 	  method: 'DELETE',
 	  path: '/{id}',
-	  handler: function( req, res ){
-	    Appointment.findOne({_id: req.params.id}, function( err, apt ){
-	      if( err ) {
-		res( err );
-	        return;
-	      }
-	      var request = {
-			headers: req.headers, 
-	                method: "DELETE",
-			payload: req.payload,
-			createdAt: new Date().toISOString(),
-	        id: apt._id,
-	      }
-	      apt.requests.push( request );
-	      apt.save();
-	      fs.readFile('views/request.html', 'utf8', function (err,data) {
-		if (err) {
-		  return console.log(err);
-		}
-		var template = Handlebars.compile(data);
-		var r = template( request );
-		io.to( req.params.id ).emit('request', r );
-	      });
-	      res( request )
-	    })
-	  }
+	  handler: handler
 	}
-]
+    ]
 }
