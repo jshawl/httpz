198baa82368c6db3b225307d0604fa2e13c4bf89
Jesse Shawl <jesse@jshawl.com>
2015-02-28 07:51:50 -0500

    work on proxy

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,27 @@
 var socket = io();
+var shouldProxy = document.getElementById('proxy');
+var proxyPort = document.getElementById('port');
+
 socket.on('request', function (data) {
   $('.requests').prepend( data );
   render();
 });
+
+socket.on('proxy', function (data) {
+  console.log( data );
+  if( shouldProxy.checked ){
+    proxy( data, port.value );
+  }
+});
+
+var proxy = function proxy( payload, port ){
+  $.ajax({
+    type: "POST",
+    async: true,
+    data: payload,
+    url: 'http://localhost:' + port
+  });
+}
 
 var render = function render(){
   $('.js-timeago').timeago();
@@ -47,4 +66,25 @@ $('.js-try-it').on('click', function( event ){
   event.preventDefault();
   var toEval = $('.js-try-it-code').html();
   eval( toEval );
+});
+
+$('.js-test-connection').on('click', function( e ){
+  var $res = $('.js-connection-result');
+  $.ajax({
+    type: "POST",
+    async: true,
+    url: 'http://localhost:' + port.value,
+    success: function(message,text,response){
+      $res.attr('data-status','success')
+      $res.html( "Ok" );
+    }, 
+    error: function( xhr, status, err ){
+      $res.attr('data-status','error')
+      if( xhr.readyState == 0 ){
+	$res.html( "Connection refused.");
+      } else{
+        $res.html( err );
+      }
+    }
+  });
 });
