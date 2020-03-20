8f753230fdefb56ce2e7226360e8c5348374aa99
Jesse Shawl <jesse@jesse.sh>
2020-03-20 17:04:58 -0500

    clean server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,14 @@
-var Path = require('path');
 var express = require('express');
 require('./config/db.js')();
-var hbs = require('./config/handlebars')
 var app = express()
 var bodyParser = require('body-parser')
 var cors = require("cors")
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.set("view engine", "hbs")
-app.use(express.static('public'))
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-
 var Appointment = require('./models/appointment.js');
 
 io.sockets.on('connection', function(socket) {
@@ -34,34 +29,10 @@ var handler = (io) => (req, res) => {
   app[method.toLowerCase()]("/:id", handler(io))
 })
 
-app.get('/', (req,res)=> res.render("index"))
-
-const getAppointment = (id, callback) => {
-  Appointment.findOne({ _id: id }, function(err, apt) {
-    console.log(err, apt)
-    if (err) return callback(err)
-    callback(apt)
-  })
-}
-
 app.get('/:id.json', (req, res) => {
-  getAppointment(req.params.id, (apt => res.json(apt)))
-})
-
-app.get('/:id',(req, res)=> {
-  if (req.query.challenge) {
-    reply(req.query.challenge)
-  } else if (JSON.stringify(req.query) != "{}") {
-    handler(req, res)
-  } else {
-    Appointment.findOne({ _id: req.params.id }, function(err, apt) {
-      console.log(err,apt)
-      if (err) return res.json(err)
-      apt.requests = apt.requests.reverse()
-      apt.host = req.headers.host;
-      res.render('show', { data: apt, scheme: process.env.NODE_ENV === "production" ? "https" : "http" });
-    })
-  }
+  Appointment.findOne({ _id: req.params.id }, function(err, apt) {
+    res.json(err || apt)
+  })
 })
 
 app.get("/appointments/create.json", (req, res) => {
@@ -69,16 +40,6 @@ app.get("/appointments/create.json", (req, res) => {
     createdAt: new Date()
   })
   apt.save((err, apt) => res.json(err || apt))
-})
-
-app.get("/appointments/create", (req, res) => {
-  var apt = new Appointment({
-    createdAt: new Date()
-  })
-  apt.save(function(err, apt) {
-    if (err) return res(err);
-    res.redirect('/' + apt._id);
-  })
 })
 
 server.listen(process.env.PORT || 3030)
