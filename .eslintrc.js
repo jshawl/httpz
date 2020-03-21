6f277bbd1783afc3f2ab4d49038251e06fbf2dc2
Jesse Shawl <jesse@jesse.sh>
2020-03-21 13:36:24 -0500

    yarn format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  ignorePatterns: ["node_modules/"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
-    },
-    ignorePatterns: ["node_modules/"],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
-    plugins: ["react"],
-    rules: {
-        "react/prop-types": 0,
-        "react/no-unescaped-entities": 0,
-        "react/display-name": 0
-    }
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "react/display-name": 0
+  }
 };
