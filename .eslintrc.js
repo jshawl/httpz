4c2d7267631402668cceafebfdb2607e8d9b700e
Jesse Shawl <jesse@jesse.sh>
2020-03-21 13:34:38 -0500

    tidy lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  ignorePatterns: ["node_modules/"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es6: true,
+        node: true
     },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  rules: {}
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    ignorePatterns: ["node_modules/"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        "react/prop-types": 0,
+        "react/no-unescaped-entities": 0,
+        "react/display-name": 0
+    }
 };
