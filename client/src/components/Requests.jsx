9804b82f68edf8113a21e73b70369d37c98cc19a
Jesse Shawl <jesse@jesse.sh>
2020-03-22 03:50:14 -0500

    wip mobile usability enhancements

--- client/src/components/Requests.jsx
+++ client/src/components/Requests.jsx
@@ -1,41 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "./Requests.scss";
-import Timeago from "react-timeago";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import * as timeago from "timeago.js";
 
-const Requests = ({ data, active, ts, appointmentId }) => (
-  <div className="Requests">
-    <ul>
-      <TransitionGroup>
+const Requests = ({ data, active, ts, appointmentId }) => {
+  const [redirect, setRedirect] = React.useState(null);
+  return (
+    <div className="Requests">
+      {redirect && <Redirect to={redirect} />}
+      <select
+        onChange={e => setRedirect(`/${appointmentId}/${e.target.value}`)}
+      >
         {data.map(datum => (
-          <CSSTransition timeout={0} key={datum.createdAt}>
-            <li
-              data-key={datum.createdAt}
-              key={datum.createdAt}
-              className={
-                active &&
-                active.createdAt === datum.createdAt &&
-                ts !== "new" &&
-                "active"
-              }
-            >
-              <Link to={`/${appointmentId}/${datum.createdAt}`}>
-                <pre>
-                  {datum.method} /{datum.id}
-                </pre>
-                <pre className="soft">{datum.headers["user-agent"]}</pre>
-                <Timeago date={datum.createdAt} />
-              </Link>
-            </li>
-          </CSSTransition>
+          <option
+            value={datum.createdAt}
+            key={datum.createdAt}
+            defaultValue={
+              active && active.createdAt === datum.createdAt && ts !== "new"
+            }
+          >
+            {datum.method} /{datum.id}
+            {timeago.format(new Date(datum.createdAt))}
+          </option>
+        ))}
+        <option value="new">Try it</option>
+      </select>
+      <ul style={{ display: "none" }}>
+        {data.map(datum => (
+          <li
+            data-key={datum.createdAt}
+            key={datum.createdAt}
+            className={
+              active &&
+              active.createdAt === datum.createdAt &&
+              ts !== "new" &&
+              "active"
+            }
+          >
+            <Link to={`/${appointmentId}/${datum.createdAt}`}>
+              <pre>
+                {datum.method} /{datum.id}
+              </pre>
+              <pre className="soft">{datum.headers["user-agent"]}</pre>
+              <span> date={datum.createdAt}</span>
+            </Link>
+          </li>
         ))}
         <li className={ts === "new" ? "active" : ""}>
           <Link to={"/" + appointmentId + "/new"}>Try it</Link>
         </li>
-      </TransitionGroup>
-    </ul>
-  </div>
-);
+      </ul>
+    </div>
+  );
+};
 
 export default Requests;
