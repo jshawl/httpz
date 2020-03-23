91ef2496f4860f216b56979f58fb7336829e4249
Jesse Shawl <jesse@jesse.sh>
2020-03-22 19:31:04 -0500

    add logging

---
+++ server/logger.js
@@ -0,0 +1,21 @@
+var winston = require("winston");
+var expressWinston = require("express-winston");
+require("winston-syslog");
+
+var winstonPapertrail = new winston.transports.Syslog({
+  host: process.env.SYSLOG_HOST,
+  port: process.env.SYSLOG_PORT,
+  localhost: "httpz.app",
+  app_name: "api",
+});
+
+module.exports = expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+    winstonPapertrail
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+})
