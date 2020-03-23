2c520617b7422a5864d67caa2be240da30ca98d8
Jesse Shawl <jesse@jesse.sh>
2020-03-22 19:32:08 -0500

    for mat n lint

--- server/logger.js
+++ server/logger.js
@@ -6,16 +6,13 @@ var winstonPapertrail = new winston.transports.Syslog({
   host: process.env.SYSLOG_HOST,
   port: process.env.SYSLOG_PORT,
   localhost: "httpz.app",
-  app_name: "api",
+  app_name: "api"
 });
 
 module.exports = expressWinston.logger({
-  transports: [
-    new winston.transports.Console(),
-    winstonPapertrail
-  ],
+  transports: [new winston.transports.Console(), winstonPapertrail],
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.json()
   )
-})
+});
